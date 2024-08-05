a14deb05d9bb7869dc4ff4d3451d6d1b7fdbe552
stryngs <support@configitnow.com>
2024-08-05 16:04:08 -0400

    Added PSK and multiple client option

--- README.md
+++ README.md
@@ -2,24 +2,26 @@
 A wrapper for creating on-the-fly WireGuard configurations
 
 # What does it do?
-This code created the files necessary for a quick setup server and client side for wireguard.
+This code created the files necessary for a quick wireguard setup.
 
-The default IP range built into this is for a 10.249.177.1 as the server and a 10.249.177.2 as the client.  Lines 38 and 44 will be of interest in this regard.
+# Single client setup.
+The default IP range built into this is for a 10.249.177.1 as the server and a 10.249.177.2 as the client.
+
+# Multiple client setup.
+Uses the same layout on the server side as the single client setup with the addition of being able to generate configurations for multiple clients.  This is achieved by using `-m` and providing a number from 3 to 254.  As an example if you did `-m 10` then you would create 9 different client files.  The ip address granted to cliWire-4.conf would be a `10.249.177.4`.
 
 # How?
 ```
 ## Server setup:
-    ip link add dev svrWire type wireguard
-    ip address add dev svrWire 10.249.177.1/24
-    wg setconf svrWire ./svrWire.conf
-    ifconfig svrWire up
+    wg-quick up ./svrWire.conf
+    ifconfig svrWire 10.249.177.1 up
 
 ## Client setup:
     wg-quick up ./cliWire.conf
 
 ## Server teardown:
-    ip link delete svrWire
+    wg-quick down ./svrWire.conf
 
 ## Client teardown:
-    ip link delete cliWire
+    wg-quick down ./cliWire.conf
 ```
--- wireMaker.py
+++ wireMaker.py
@@ -3,55 +3,92 @@
 import argparse
 import os
 
-def main(endIp, endPt):
-
-    ## Key fun
-    os.system('umask 077 && wg genkey > svrPrvkey')
-    os.system('cat svrPrvkey | wg pubkey > svrPubkey')
+def peerGen(endIp, endPt, svrPub, cliOct):
+    """Generate and return peer information"""
     os.system('umask 077 && wg genkey > cliPrvkey')
     os.system('cat cliPrvkey | wg pubkey > cliPubkey')
-    # os.system('wg genpsk > psk 2>/dev/null')
-
-    ## Shh
-    with open('svrPrvkey') as iFile:
-        svrPriv = iFile.read().splitlines()[0]
-    with open('svrPubkey') as iFile:
-        svrPub = iFile.read().splitlines()[0]
+    os.system('wg genpsk > psk 2>/dev/null')
     with open('cliPrvkey') as iFile:
         cliPriv = iFile.read().splitlines()[0]
     with open('cliPubkey') as iFile:
         cliPub = iFile.read().splitlines()[0]
-    # with open('psk') as iFile:
-    #     psk = iFile.read().splitlines()[0]
-
-    ## quick setups
-    svrQck = f"""[Interface]
-PrivateKey = {svrPriv}
-ListenPort = {endPt}
-
-[Peer]
-PublicKey = {cliPub}
-AllowedIPs = 10.249.177.2/32
-"""
+    with open('psk') as iFile:
+        psk = iFile.read().splitlines()[0]
+    cliIp = f'Address = 10.249.177.{cliOct}/24'
     cliQck = f"""[Interface]
 PrivateKey = {cliPriv}
-Address = 10.249.177.2/24
-DNS = 9.9.9.9, 1.1.1.1
+{cliIp}
 
 [Peer]
 PublicKey = {svrPub}
 Endpoint = {endIp}:{endPt}
 AllowedIPs = 10.249.177.0/24
+PresharedKey = {psk}
 """
-    with open('svrWire.conf', 'w') as oFile:
-        oFile.write(f'{svrQck}')
-    with open('cliWire.conf', 'w') as oFile:
-        oFile.write(f'{cliQck}')
+    return cliQck, cliPub, psk, cliOct
+
+
+def main(endIp, endPt, multiClient = False):
+    os.system('umask 077 && wg genkey > svrPrvkey')
+    os.system('cat svrPrvkey | wg pubkey > svrPubkey')
+    with open('svrPrvkey') as iFile:
+        svrPriv = iFile.read().splitlines()[0]
+    with open('svrPubkey') as iFile:
+        svrPub = iFile.read().splitlines()[0]
+
+    ## Create environment for one client
+    if multiClient is False:
+        cliQck = peerGen(endIp, endPt, svrPub, 2)
+        svrQck = f"""[Interface]
+PrivateKey = {svrPriv}
+ListenPort = {endPt}
+
+[Peer]
+PublicKey = {cliQck[1]}
+AllowedIPs = 10.249.177.2/32
+PresharedKey = {cliQck[2]}
+"""
+
+        with open('svrWire.conf', 'w') as oFile:
+            oFile.write(f'{svrQck}')
+        with open('cliWire.conf', 'w') as oFile:
+            oFile.write(f'{cliQck[0]}')
+
+    ## Create environment for multiple clients
+    else:
+        pList = []
+        for i in range (2, multiClient + 1):
+            pList.append(peerGen(endIp, endPt, svrPub, i))
+
+        fList = []
+        for p in pList:
+            cStr = f"""[Peer]
+PublicKey = {p[1]}
+AllowedIPs = 10.249.177.{p[3]}/32
+PresharedKey = {p[2]}
+"""
+            fList.append(cStr)
+
+        cliStr = '\n'.join(fList)
+        svrQck = f"""[Interface]
+PrivateKey = {svrPriv}
+ListenPort = {endPt}
+
+{cliStr}
+"""
+        with open('svrWire.conf', 'w') as oFile:
+            oFile.write(f'{svrQck}')
+
+        ## Generate client configs
+        for i in range(len(pList)):
+            with open(f'cliWire-{i + 2}.conf', 'w') as oFile:
+                oFile.write(f'{pList[i][0]}')
 
 if __name__ == '__main__':
 
     ## Inputs
     psr = argparse.ArgumentParser(description = 'A wrapper for creating WireGuard configurations')
+    psr.add_argument('-m', help = 'Multi client mode')
     psr.add_argument('-s', help = 'WireGuard server endpoint IP address', required = True)
     psr.add_argument('-p', help = '51820 is the default port')
     args = psr.parse_args()
@@ -62,10 +99,15 @@ if __name__ == '__main__':
         endPt = 51820
     else:
         endPt = args.p
+    if args.m is not None:
+        args.m = int(args.m)
+        x = main(endIp, endPt, args.m)
+    else:
+        main(endIp, endPt)
 
-    ## Run
-    main(endIp, endPt)
+    ## Cleanup    
     os.remove('cliPrvkey')
     os.remove('cliPubkey')
     os.remove('svrPrvkey')
     os.remove('svrPubkey')
+    os.remove('psk')
